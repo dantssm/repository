53560692740953ea26584d11b0ee51c7c936fbbf
Danylo <149315521+dantssm@users.noreply.github.com>
2024-04-08 10:53:33 +0300

    correction 'if node is None'

--- Convert a linked list to a string/the file.py
+++ Convert a linked list to a string/the file.py
@@ -1,6 +1,8 @@
 def stringify(node):
     def helper(node2):
         return node2.data, node2.next
+    if node is None:
+        return 'None'
     string = ''
     data, node = helper(node)
     while not node is None:
