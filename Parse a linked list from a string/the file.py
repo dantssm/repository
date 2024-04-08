ff56bb464d59e0b1af8d4ac63f9f957e8e46fc62
Danylo <149315521+dantssm@users.noreply.github.com>
2024-04-08 11:27:55 +0300

    initial commit

---
+++ Parse a linked list from a string/the file.py
@@ -0,0 +1,13 @@
+def parse(string):
+    if string == 'None':
+        return None
+    values = string.split(' -> ')
+    head = Node(int(values[0]))
+    current = head
+    for node in values[:-1]:
+        current.next = Node(int(node))
+        current = current.next
+    return head
+
+def linked_list_from_string(s):
+    return # head to linked list
