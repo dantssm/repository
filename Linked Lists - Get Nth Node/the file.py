8707b78db22f24bd6191dd51665f8d1f3c1058ba
Danylo <149315521+dantssm@users.noreply.github.com>
2024-04-08 11:49:33 +0300

    initial commit

---
+++ Linked Lists - Get Nth Node/the file.py
@@ -0,0 +1,25 @@
+from preloaded import Node
+
+# class Node(object):
+#     """Node class for reference"""
+#     def __init__(self, data, next=None):
+#         self.data = data
+#         self.next = next
+def stringify(node):
+    def helper(node2):
+        return node2.data, node2.next
+    if node is None:
+        return 'None'
+    string = ''
+    data, node = helper(node)
+    while not node is None:
+        string += f'{data} -> '
+        data, node = helper(node)
+    string += f'{data} -> '
+    string += 'None'
+    return string
+
+def get_nth(node, index):
+    node = stringify(node)
+    node = node.split(' -> ')
+    return Node(node[index])
