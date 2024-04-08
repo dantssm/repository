d3a157228dea63c1c2d2715e9e458ad07ec58961
Danylo <149315521+dantssm@users.noreply.github.com>
2024-04-08 22:00:43 +0300

    corrected order of assignment

--- Swap Node Pairs In Linked List/the file.py
+++ Swap Node Pairs In Linked List/the file.py
@@ -4,6 +4,5 @@ def swap_pairs(head):
     if not head or not head.next:
         return head
     new_head = head.next
-    head.next = swap_pairs(new_head)
-    new_head.next = head
+    new_head.next, head.next = head, swap_pairs(new_head.next)
     return new_head
