1872436fb9de0eb073354a608e1d24cde3a5129c
Danylo <149315521+dantssm@users.noreply.github.com>
2024-04-08 12:53:15 +0300

    fixed parse function to save float numbers and added a track if the data was added to the list or not

--- Linked Lists - Sorted Insert/the file.py
+++ Linked Lists - Sorted Insert/the file.py
@@ -22,10 +22,10 @@ def parse(string):
     if string == 'None':
         return None
     values = string.split(' -> ')
-    head = Node(int(values[0]))
+    head = Node(float(values[0]))
     current = head
     for node in values[1:-1]:
-        current.next = Node(int(node))
+        current.next = Node(float(node))
         current = current.next
     return head
 
@@ -33,12 +33,18 @@ def sorted_insert(head, data):
     list = stringify(head)
     if list == 'None':
         return Node(data)
-    list = list.split(' -> ')[:1]
+    list = list.split(' -> ')[:-1]
+    bl = False
     for i, el in enumerate(list):
-        if data > int(el):
+        if data < int(el):
             list = list[:i] + [str(data)] + list[i:]
+            bl = True
             break
         continue
+    if not bl:
+        list.append(str(data))
+    list.append('None')
     list = ' -> '.join(list)
+    print(list)
     list = parse(list)
     return list
