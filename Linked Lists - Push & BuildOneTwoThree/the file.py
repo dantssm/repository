2c0fa8cefa18b59aaf486e484250d6b5e176744b
Danylo <149315521+dantssm@users.noreply.github.com>
2024-04-08 11:40:02 +0300

    initial commit

---
+++ Linked Lists - Push & BuildOneTwoThree/the file.py
@@ -0,0 +1,20 @@
+from preloaded import Node
+
+'''
+Node is defined in preloaded like this:
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+'''
+    
+def push(head, data):
+    print(data, head)
+    return Node(data, head)
+
+def build_one_two_three():
+    linked_list = Node(3)
+    linked_list = push(linked_list, 2)
+    linked_list = push(linked_list, 1)
+    return linked_list
